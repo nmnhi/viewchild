cf43c6dd8d979dee29059d74b2f5983a8abd6296
Nhi Nguyen <minhnhi08061105@gmail.com>
2022-10-18 21:49:29 +0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { DemoComponent } from './demo/demo.component';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'viewchild';
+  @ViewChild('dobInput')
+  dateOfBirth!: ElementRef;
+  @ViewChild('ageInput')
+  age!: ElementRef;
+  @ViewChild(DemoComponent, { static: true }) demoComp!: DemoComponent;
+  calculateAge() {
+    let birthYear = new Date(
+      this.dateOfBirth.nativeElement.value
+    ).getFullYear();
+    let currentYear = new Date().getFullYear();
+    let age = currentYear - birthYear;
+    this.age.nativeElement.value = age;
+  }
 }
